50b4e6824941c4ca5a49cd0f9022503bda08b904
ss473901 <satoshi.473901.1205@gmail.com>
2021-12-15 11:30:53 +0900

    add

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  console.log(input.value);
+});
