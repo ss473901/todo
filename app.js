2ffbe85bb327d4fdb68e9c1cb4590557c4749074
ss473901 <satoshi.473901.1205@gmail.com>
2021-12-17 14:49:48 +0900

    Add app.js

--- app.js
+++ app.js
@@ -1,68 +1,25 @@
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const ul = document.getElementById("ul");
+const form = document.getElementById("form"); //1.フォームタグの値を取得
+const input = document.getElementById("input"); //2.インプットタグの値を取得
+const ul = document.getElementById("ul");//8.ulタグに関しての処理をしたいのでulタグの値を取得
 
-const todos = JSON.parse(localStorage.getItem("todos"));
-
-if(todos){
-  todos.forEach(todo => {
-    add(todo);
-  })
-}
-
-form.addEventListener("submit",function(event){
-  event.preventDefault();
-  console.log(input.value);
-  add();
+form.addEventListener("submit",function(event){ //3.サブミットを押した時に、発生するイベント=functionを作成
+  event.preventDefault();  //5.サブミットを押すとデフォルトでリロードされてしまうので、デフォのイベントを妨げる
+  console.log(input.value); //4.サブミットを押した時にインプットタグの中のバリューを取得
+  add();//6.取得したバリューをもとにイベントを発生させる。先に関数名を決める
 });
 
-function add(todo) {
-  let todoText = input.value;
 
-  if(todo){
-    todoText = todo.text;
+function add(){ //7.先に設定した関数名addの　function=機能　を記述
+  let todoText = input.value; ////14.if分を書くために、input.valueを todoText と変数宣言で簡略化する
+
+  if(todoText.length > 0){ ////16.文字数が0よりも大きかったら true 以下の処理をする
+    const li = document.createElement("li"); //9.liタグの値をもとに機能を作るためにまずは定数liを作成
+    li.innerText = todoText//10.liの内部のテキストに、ユーザーがform > inputに入力した値を入れる ////15.変数宣言したtodoTextに変更
+    li.classList.add("list-group-item");//11.生成するliタグのクラスを追加
+    ul.appendChild(li);//12.liタグを　3行目で取得してきたulタグ　の子要素にする
+    input.value = "";//13.form > input　の中身を次に記入するときのために空にする
   }
-
-  if (todoText){
-    const li = document.createElement("li");
-    li.innerText = todoText;
-    li.classList.add("list-group-item");
-
-    if(todo && todo.completed){
-      li.classList.add("text-decoration-line-through");
-    }
-
-    li.addEventListener("contextmenu", function
-    (event){
-      event.preventDefault();
-      li.remove();
-      saveDate();
-    });
-
-    li.addEventListener("click",function(){
-      li.classList.toggle("text-decoration-line-through");
-      saveDate();
-    });
-    ul.appendChild(li);
-    input.value = "";
-    saveDate();
-  }
-
 }
 
 
-function saveDate(){
-  const lists = document.querySelectorAll("li");
-  let todos =[];
-
-  lists.forEach(list => {
-    let todo = {
-      text: list.innerText,
-      completed: list.classList.contains("text-decoration-line-through")
-    };
-    todos.push(todo);
-  });
-  localStorage.setItem("todos",JSON.stringify(todos));
-}
-
-
+////14　からは　42:00〜
